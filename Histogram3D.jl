e22609e028ed74f35ba6e8534eb2755b3a3e3b11
Djun0 <73646479+Djun0@users.noreply.github.com>
2024-07-12 13:31:19 +0700

    Create Histogram3D.jl

---
+++ Histogram3D.jl
@@ -0,0 +1,20 @@
+using GLMakie
+function histogram3D(x1, y2, n)
+    x, y, z = peakss(x1, y2, n)
+    δx = (x[2] - x[1]) / 2
+    δy = (y[2] - y[1]) / 2
+    fig = Figure(size=(2000, 1600), fontsize=30,px_per_unit = 2)
+    ax2 = Axis3(fig[1, 1]; aspect=(1, 1, 1), perspectiveness=0.5,xlabel=L"X_0",ylabel=L"X_1",zlabel="Số mẫu",zlabelposition[] = (0.5, 1.1))
+    rectMesh = Rect3f(Vec3f(-0.5, -0.5, 0), Vec3f(1, 1, 1))
+    meshscatter!(ax2, x, y, 0 * z; marker=rectMesh, color=:cyan,
+        markersize=Vec3f.(2δx, 2δy, z[:]),
+        shading=MultiLightShading, transparency=false)
+    for (idx, i) in enumerate(x)
+        for (idy, j) in enumerate(y)
+            rectMesh = Rect3f(Vec3f(i - δx, j - δy, 0), Vec3f(2δx, 2δy, z[idx, idy]))
+            recmesh = GeometryBasics.mesh(rectMesh)
+            lines!(ax2, recmesh, color=:black, linewidth=0.5)
+        end
+    end
+    fig
+end
