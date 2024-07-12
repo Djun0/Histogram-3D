0aed456d076e03473ebffd6b5b1f04c4d1658f31
Djun0 <73646479+Djun0@users.noreply.github.com>
2024-07-12 14:33:46 +0700

    Update Histogram3D.jl

--- Histogram3D.jl
+++ Histogram3D.jl
@@ -55,7 +55,7 @@ function histogram3D(x, y, n)
     δx = (x[2] - x[1]) / 2
     δy = (y[2] - y[1]) / 2
     fig = Figure(size=(2000, 1600), fontsize=30,px_per_unit = 2)
-    ax2 = Axis3(fig[1, 1]; aspect=(1, 1, 1), perspectiveness=0.5,xlabel=L"X_0",ylabel=L"X_1",zlabel="Số mẫu",zlabelposition[] = (0.5, 1.1))
+    ax2 = Axis3(fig[1, 1]; aspect=(1, 1, 1), perspectiveness=0.5,xlabel=L"X_0",ylabel=L"X_1",zlabel="Sample")
     rectMesh = Rect3f(Vec3f(-0.5, -0.5, 0), Vec3f(1, 1, 1))
     meshscatter!(ax2, x, y, 0 * z; marker=rectMesh, color=:cyan,
         markersize=Vec3f.(2δx, 2δy, z[:]),
